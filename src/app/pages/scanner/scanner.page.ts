93c1a6ebebcb1a4b1d760675c64a04f34756b2bc
melcv1 <0himel.cv@gmail.com>
2020-07-29 19:51:20 -0500

    cercanos
    cercanos

--- src/app/pages/scanner/scanner.page.ts
+++ src/app/pages/scanner/scanner.page.ts
@@ -10,6 +10,7 @@ export class ScannerPage {
   displayString: any[] = [];
   devices: any[] = [];
   dispositivos1: dispositivos[] = [];
+  cercanos:dispositivos[]=[];
 
   constructor(private ble: BLE, private ngZone: NgZone) {
   }
@@ -18,6 +19,7 @@ export class ScannerPage {
     this.devices = [];
     this.displayString = [];
     this.dispositivos1 = [];
+    this.cercanos=[];
     this.ble.scan([], 15).subscribe(
       device => this.onDeviceDiscovered(device)
     );
@@ -29,12 +31,30 @@ export class ScannerPage {
       this.devices.push(device)
       let aux = "";
       console.log(device)
+      
       if (device.rssi > -51) {
         this.displayString[this.devices.length - 1] = ("Muy cerca");
         aux = ("Muy cerca");
+        let cercania: dispositivos;
+        cercania = {
+          name: device.name,
+          id: device.id,
+          rssi: device.rssi,
+          cercania: aux
+        };
+        this.cercanos.push(cercania);
+
       } else if (device.rssi > -65) {
         this.displayString[this.devices.length - 1] = ("Cerca");
         aux = ("Cerca");
+        let cercania: dispositivos;
+        cercania = {
+          name: device.name,
+          id: device.id,
+          rssi: device.rssi,
+          cercania: aux
+        };
+        this.cercanos.push(cercania);
       } else {
         this.displayString[this.devices.length - 1] = ("Lejos");
         aux = ("Lejos");
@@ -48,7 +68,6 @@ export class ScannerPage {
       };
       this.dispositivos1.push(dispositivo);
     })
-
   }
 
 }
